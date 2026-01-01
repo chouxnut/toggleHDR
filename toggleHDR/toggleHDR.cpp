fdaab4cf46cf520271f99fd90a0e790331e6f862
chouxnut <tmpmail29@proton.me>
2026-01-01 04:39:34 +0000

    Update toggleHDR.cpp

--- toggleHDR/toggleHDR.cpp
+++ toggleHDR/toggleHDR.cpp
@@ -2,14 +2,13 @@
 #include <shellapi.h>
 #include <string.h>
 
-#pragma comment(lib, "User32.lib")
-#pragma comment(lib, "Shell32.lib")
-
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     int open_settings = 1;
-    for (int i = 1; i < argc; ++i)
+    for (int i = 1; i < argc; ++i) {
         if (strcmp(argv[i], "--no-icc") == 0)
             open_settings = 0;
+    }
 
     UINT32 pathCount = 0, modeCount = 0;
     if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pathCount, &modeCount) != ERROR_SUCCESS)
@@ -18,13 +17,16 @@ int main(int argc, char** argv) {
     auto* paths = new DISPLAYCONFIG_PATH_INFO[pathCount];
     auto* modes = new DISPLAYCONFIG_MODE_INFO[modeCount];
 
-    if (QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pathCount, paths, &modeCount, modes, nullptr) != ERROR_SUCCESS)
+    if (QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pathCount, paths, &modeCount, modes, nullptr) != ERROR_SUCCESS) {
+        delete[] paths;
+        delete[] modes;
         return 1;
+    }
 
     for (UINT32 i = 0; i < pathCount; ++i) {
         const auto& t = paths[i].targetInfo;
 
-        DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO g{};
+        DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO g = {};
         g.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO;
         g.header.size = sizeof(g);
         g.header.adapterId = t.adapterId;
@@ -33,39 +35,35 @@ int main(int argc, char** argv) {
         if (DisplayConfigGetDeviceInfo(&g.header) != ERROR_SUCCESS)
             continue;
 
-        BOOL turnOn = !g.advancedColorEnabled;
+        BOOL turnOn = g.advancedColorEnabled ? FALSE : TRUE;
 
 #if defined(DISPLAYCONFIG_DEVICE_INFO_SET_HDR_STATE)
-        DISPLAYCONFIG_SET_HDR_STATE s{};
+        DISPLAYCONFIG_SET_HDR_STATE s = {};
         s.header.type = DISPLAYCONFIG_DEVICE_INFO_SET_HDR_STATE;
         s.header.size = sizeof(s);
         s.header.adapterId = t.adapterId;
         s.header.id = t.id;
-        s.enableHdr = turnOn;
+        s.enableHdr = turnOn ? 1u : 0u;
         DisplayConfigSetDeviceInfo(&s.header);
 #else
-        DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{};
+        DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s = {};
         s.header.type = DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE;
         s.header.size = sizeof(s);
         s.header.adapterId = t.adapterId;
         s.header.id = t.id;
-        s.enableAdvancedColor = turnOn;
+        s.enableAdvancedColor = turnOn ? 1u : 0u;
         DisplayConfigSetDeviceInfo(&s.header);
 #endif
 
         if (open_settings) {
-            SHELLEXECUTEINFOA sei{};
-            sei.cbSize = sizeof(sei);
-            sei.fMask = SEE_MASK_NOCLOSEPROCESS;
-            sei.lpVerb = "open";
-            sei.lpFile = "ms-settings:display";
-            sei.nShow = SW_SHOWMINNOACTIVE;
-
-            if (ShellExecuteExA(&sei) && sei.hProcess) {
-                Sleep(1200);
-                TerminateProcess(sei.hProcess, 0);
-                CloseHandle(sei.hProcess);
-            }
+            ShellExecuteW(
+                nullptr,
+                L"open",
+                L"ms-settings:display",
+                nullptr,
+                nullptr,
+                SW_SHOWNOACTIVATE
+            );
         }
 
         break;
@@ -74,4 +72,13 @@ int main(int argc, char** argv) {
     delete[] paths;
     delete[] modes;
     return 0;
+}
+
+int WINAPI WinMain(
+    HINSTANCE,
+    HINSTANCE,
+    LPSTR,
+    int
+) {
+    return main(__argc, __argv);
 }
