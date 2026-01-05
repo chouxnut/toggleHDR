fbba430b4206de9141a31db1b5c8a1a2da909c62
chouxnut <tmpmail29@proton.me>
2026-01-05 02:50:34 +0000

    Update toggleHDR.cpp

--- toggleHDR/toggleHDR.cpp
+++ toggleHDR/toggleHDR.cpp
@@ -3,21 +3,29 @@
 #pragma comment(lib,"User32.lib")
 #pragma comment(lib,"Shell32.lib")
 
-static HWINEVENTHOOK h; static bool d;
+static HWND target;
 
-void CALLBACK W(HWINEVENTHOOK, DWORD e, HWND w, LONG, LONG, DWORD, DWORD){
-    wchar_t c[32], x[MAX_PATH]; DWORD p,l=MAX_PATH;
-    if(d||!w||!GetClassNameW(w,c,32)||wcscmp(c,L"ApplicationFrameWindow")) return;
-    GetWindowThreadProcessId(w,&p);
+BOOL CALLBACK Find(HWND h, LPARAM){
+    wchar_t c[64];
+    GetClassNameW(h,c,64);
+    if(wcscmp(c,L"ApplicationFrameWindow")) return TRUE;
+
+    DWORD p; GetWindowThreadProcessId(h,&p);
     HANDLE P=OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION,0,p);
-    if(P&&QueryFullProcessImageNameW(P,0,x,&l)&&wcsstr(x,L"SystemSettings.exe")){
-        PostMessageW(w,WM_CLOSE,0,0); d=1; UnhookWinEvent(h);
-    }
-    if(P) CloseHandle(P);
+    if(!P) return TRUE;
+
+    wchar_t x[MAX_PATH]; DWORD l=MAX_PATH;
+    bool ok = QueryFullProcessImageNameW(P,0,x,&l) &&
+              wcsstr(x,L"SystemSettings.exe");
+    CloseHandle(P);
+
+    if(ok){ target=h; return FALSE; }
+    return TRUE;
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
-    UINT32 pc=0,mc=0; GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS,&pc,&mc);
+    UINT32 pc=0,mc=0;
+    GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS,&pc,&mc);
     auto p=new DISPLAYCONFIG_PATH_INFO[pc];
     auto m=new DISPLAYCONFIG_MODE_INFO[mc];
     QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS,&pc,p,&mc,m,0);
@@ -30,14 +38,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
     DisplayConfigSetDeviceInfo(&s.header);
 
     if(s.enableAdvancedColor){
-        d=0;
-        h=SetWinEventHook(
-            EVENT_OBJECT_SHOW, EVENT_OBJECT_SHOW,
-            0, W, 0, 0,
-            WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS
-        );
         ShellExecuteW(0,L"open",L"ms-settings:display",0,0,SW_SHOWMINNOACTIVE);
-        MSG M; while(!d&&GetMessageW(&M,0,0,0)) DispatchMessageW(&M);
+
+        MSG M;
+        for(int i=0;i<50 && !target;i++){
+            PeekMessageW(&M,0,0,0,PM_REMOVE);
+            EnumWindows(Find,0);
+        }
+
+        if(target){
+            PostMessageW(target,WM_SYSCOMMAND,SC_CLOSE,0);
+            PostMessageW(target,WM_CLOSE,0,0);
+        }
     }
-    delete[] p; delete[] m; return 0;
+    delete[] p; delete[] m;
+    return 0;
 }
