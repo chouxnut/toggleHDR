79e922d7a3d28ea9c7a1dbc3c259de5c139cd91d
chouxnut <tmpmail29@proton.me>
2026-01-24 18:59:31 +0000

    Update toggleHDR.cpp

--- toggleHDR/toggleHDR.cpp
+++ toggleHDR/toggleHDR.cpp
@@ -2,47 +2,62 @@
 #include <shellapi.h>
 #include <vector>
 
-#pragma comment(lib,"User32.lib")
-#pragma comment(lib,"Shell32.lib")
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "Shell32.lib")
 
-DWORD pid;
-
-BOOL CALLBACK CW(HWND h, LPARAM)
+static BOOL CALLBACK CloseSettings(HWND h, LPARAM)
 {
-    DWORD p;
-    GetWindowThreadProcessId(h, &p);
-    if (p == pid) PostMessageW(h, WM_CLOSE, 0, 0);
+    wchar_t c[64];
+    GetClassNameW(h, c, 64);
+
+    if (!wcscmp(c, L"ApplicationFrameWindow")) {
+        PostMessageW(h, WM_CLOSE, 0, 0);
+        return FALSE;
+    }
     return TRUE;
 }
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+void ToggleHDR()
 {
     UINT32 pc = 0, mc = 0;
-    GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pc, &mc);
+    if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pc, &mc))
+        return;
 
     std::vector<DISPLAYCONFIG_PATH_INFO> p(pc);
     std::vector<DISPLAYCONFIG_MODE_INFO> m(mc);
-    QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pc, p.data(), &mc, m.data(), 0);
+
+    if (QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pc, p.data(), &mc, m.data(), nullptr))
+        return;
 
     auto& t = p[0].targetInfo;
 
     DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO g{};
-    g.header = { DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO, sizeof(g), t.adapterId, t.id };
-    DisplayConfigGetDeviceInfo(&g.header);
+    g.header = {
+        DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO,
+        sizeof(g),
+        t.adapterId,
+        t.id
+    };
 
-    DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{};
-    s.header = { DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE, sizeof(s), t.adapterId, t.id };
-    s.enableAdvancedColor = !g.advancedColorEnabled;
-    DisplayConfigSetDeviceInfo(&s.header);
+    if (!DisplayConfigGetDeviceInfo(&g.header)) {
+        DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{};
+        s.header = {
+            DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE,
+            sizeof(s),
+            t.adapterId,
+            t.id
+        };
+        s.enableAdvancedColor = !g.advancedColorEnabled;
+        DisplayConfigSetDeviceInfo(&s.header);
+    }
 
-    SHELLEXECUTEINFOW e{ sizeof(e), SEE_MASK_NOCLOSEPROCESS };
-    e.lpVerb = L"open";
-    e.lpFile = L"ms-settings:display";
-    e.nShow = SW_SHOWMINNOACTIVE;
-    ShellExecuteExW(&e);
+    ShellExecuteW(nullptr, L"open", L"ms-settings:display", nullptr, nullptr, SW_SHOWMINNOACTIVE);
+    Sleep(500);
+    EnumWindows(CloseSettings, 0);
+}
 
-    pid = GetProcessId(e.hProcess);
-    WaitForInputIdle(e.hProcess, 3000);
-    EnumWindows(CW, 0);
-    CloseHandle(e.hProcess);
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    ToggleHDR();
+    return 0;
 }
