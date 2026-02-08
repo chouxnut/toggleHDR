85df96317815e9eb25421531383c13354f515bf3
chouxnut <tmpmail29@proton.me>
2026-02-08 11:50:12 +0000

    Update toggleHDR.cpp

--- toggleHDR/toggleHDR.cpp
+++ toggleHDR/toggleHDR.cpp
@@ -11,18 +11,21 @@ BOOL CALLBACK C(HWND h, LPARAM)
 {
     DWORD p;
     GetWindowThreadProcessId(h, &p);
-    if (p == pid) PostMessageW(h, WM_CLOSE, 0, 0);
+
+    if (p == pid || GetParent(GetProcessId(p)) == (HANDLE)pid)
+        PostMessageW(h, WM_CLOSE, 0, 0);
+
     return TRUE;
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
     UINT32 pc = 0, mc = 0;
-    if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pc, &mc)) return 0;
+    GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pc, &mc);
 
     std::vector<DISPLAYCONFIG_PATH_INFO> p(pc);
     std::vector<DISPLAYCONFIG_MODE_INFO> m(mc);
-    if (QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pc, p.data(), &mc, m.data(), nullptr)) return 0;
+    QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pc, p.data(), &mc, m.data(), nullptr);
 
     auto& t = p[0].targetInfo;
 
@@ -30,20 +33,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         { DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO, sizeof(g), t.adapterId, t.id }
     };
 
-    if (!DisplayConfigGetDeviceInfo(&g.header)) {
-        DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{
-            { DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE, sizeof(s), t.adapterId, t.id },
-            !g.advancedColorEnabled
-        };
-        DisplayConfigSetDeviceInfo(&s.header);
-    }
+    DisplayConfigGetDeviceInfo(&g.header);
+
+    DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{
+        { DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE, sizeof(s), t.adapterId, t.id },
+        !g.advancedColorEnabled
+    };
+
+    DisplayConfigSetDeviceInfo(&s.header);
 
     CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
     IApplicationActivationManager* a;
     if (SUCCEEDED(CoCreateInstance(
-        CLSID_ApplicationActivationManager, nullptr, CLSCTX_INPROC_SERVER,
-        IID_PPV_ARGS(&a))))
+        CLSID_ApplicationActivationManager, nullptr,
+        CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&a))))
     {
         a->ActivateApplication(
             L"windows.immersivecontrolpanel_cw5n1h2txyewy!microsoft.windows.immersivecontrolpanel",
@@ -51,8 +55,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         a->Release();
     }
 
-    Sleep(600);
-    EnumWindows(C, 0);
+    for (int i = 0; i < 10; i++) {
+        EnumWindows(C, 0);
+        Sleep(100);
+    }
 
     CoUninitialize();
     return 0;
