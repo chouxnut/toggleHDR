1bb60aa109372fc266aa6b76ac17205618e78be8
chouxnut <tmpmail29@proton.me>
2026-02-09 00:50:02 +0000

    Update toggleHDR.cpp

--- toggleHDR/toggleHDR.cpp
+++ toggleHDR/toggleHDR.cpp
@@ -1,73 +1,62 @@
 #include <windows.h>
+#include <shellapi.h>
 #include <vector>
 
 #pragma comment(lib,"User32.lib")
+#pragma comment(lib,"Shell32.lib")
 
-static void ApplyColorTriggers()
+BOOL CALLBACK CloseSettingsOnly(HWND h, LPARAM)
 {
-    SendMessageTimeoutW(
-        HWND_BROADCAST,
-        WM_SETTINGCHANGE,
-        0,
-        (LPARAM)L"Color",
-        SMTO_ABORTIFHUNG,
-        100,
-        nullptr
-    );
+    wchar_t cls[64], title[256];
 
-    SetDisplayConfig(
-        0, nullptr,
-        0, nullptr,
-        SDC_APPLY | SDC_TOPOLOGY_INTERNAL
-    );
+    if (!GetClassNameW(h, cls, 64)) return TRUE;
+    if (wcscmp(cls, L"ApplicationFrameWindow")) return TRUE;
 
-    SetICMMode(nullptr, ICM_ON);
+    GetWindowTextW(h, title, 256);
+
+    // 설정 앱 제목 필터 (한/영)
+    if (wcsstr(title, L"설정") || wcsstr(title, L"Settings"))
+        PostMessageW(h, WM_CLOSE, 0, 0);
+
+    return TRUE;
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-    UINT32 pathCount = 0, modeCount = 0;
-    if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pathCount, &modeCount))
+    UINT32 pc = 0, mc = 0;
+    if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pc, &mc))
         return 0;
 
-    std::vector<DISPLAYCONFIG_PATH_INFO> paths(pathCount);
-    std::vector<DISPLAYCONFIG_MODE_INFO> modes(modeCount);
+    std::vector<DISPLAYCONFIG_PATH_INFO> paths(pc);
+    std::vector<DISPLAYCONFIG_MODE_INFO> modes(mc);
 
-    if (QueryDisplayConfig(
-        QDC_ONLY_ACTIVE_PATHS,
-        &pathCount, paths.data(),
-        &modeCount, modes.data(),
-        nullptr))
+    if (QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pc, paths.data(),
+                           &mc, modes.data(), nullptr))
         return 0;
 
-    for (auto& path : paths)
+    auto& t = paths[0].targetInfo;
+
+    DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO g{
+        { DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO,
+          sizeof(g), t.adapterId, t.id }
+    };
+
+    if (!DisplayConfigGetDeviceInfo(&g.header))
     {
-        auto& t = path.targetInfo;
-
-        DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO g{};
-        g.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO;
-        g.header.size = sizeof(g);
-        g.header.adapterId = t.adapterId;
-        g.header.id = t.id;
-
-        if (DisplayConfigGetDeviceInfo(&g.header))
-            continue;
-
-        if (!g.advancedColorSupported)
-            continue;
-
-        DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{};
-        s.header.type = DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE;
-        s.header.size = sizeof(s);
-        s.header.adapterId = t.adapterId;
-        s.header.id = t.id;
-        s.enableAdvancedColor = !g.advancedColorEnabled;
-
-        if (!DisplayConfigSetDeviceInfo(&s.header))
-            ApplyColorTriggers();
-
-        break; // 첫 HDR 지원 디스플레이만
+        DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{
+            { DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE,
+              sizeof(s), t.adapterId, t.id },
+            !g.advancedColorEnabled
+        };
+        DisplayConfigSetDeviceInfo(&s.header);
     }
 
+    // 색상 적용 트리거
+    ShellExecuteW(nullptr, L"open", L"ms-settings:display",
+                  nullptr, nullptr, SW_SHOWMINNOACTIVE);
+
+    Sleep(600); // 이 값이 핵심 (500~800 안정권)
+
+    EnumWindows(CloseSettingsOnly, 0);
     return 0;
 }
