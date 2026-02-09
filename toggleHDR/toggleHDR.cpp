7593a8d056ba4f9b1e2429c285809786a9bd2f64
chouxnut <tmpmail29@proton.me>
2026-02-09 00:33:03 +0000

    Update toggleHDR.cpp

--- toggleHDR/toggleHDR.cpp
+++ toggleHDR/toggleHDR.cpp
@@ -1,84 +1,73 @@
 #include <windows.h>
-#include <shellapi.h>
 #include <vector>
 
 #pragma comment(lib,"User32.lib")
-#pragma comment(lib,"Shell32.lib")
 
-#define WM_TRAY (WM_USER + 1)
-NOTIFYICONDATAW nid{};
-
-void ApplyColorTriggers()
+static void ApplyColorTriggers()
 {
-    SendMessageTimeoutW(HWND_BROADCAST, WM_SETTINGCHANGE, 0,
-        (LPARAM)L"Color", SMTO_ABORTIFHUNG, 100, nullptr);
+    SendMessageTimeoutW(
+        HWND_BROADCAST,
+        WM_SETTINGCHANGE,
+        0,
+        (LPARAM)L"Color",
+        SMTO_ABORTIFHUNG,
+        100,
+        nullptr
+    );
 
-    SetDisplayConfig(0, nullptr, 0, nullptr,
-        SDC_APPLY | SDC_TOPOLOGY_INTERNAL);
+    SetDisplayConfig(
+        0, nullptr,
+        0, nullptr,
+        SDC_APPLY | SDC_TOPOLOGY_INTERNAL
+    );
 
     SetICMMode(nullptr, ICM_ON);
 }
 
-void ToggleHDR()
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-    UINT32 pc = 0, mc = 0;
-    if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pc, &mc)) return;
+    UINT32 pathCount = 0, modeCount = 0;
+    if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pathCount, &modeCount))
+        return 0;
 
-    std::vector<DISPLAYCONFIG_PATH_INFO> p(pc);
-    std::vector<DISPLAYCONFIG_MODE_INFO> m(mc);
-    if (QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &pc, p.data(), &mc, m.data(), nullptr)) return;
+    std::vector<DISPLAYCONFIG_PATH_INFO> paths(pathCount);
+    std::vector<DISPLAYCONFIG_MODE_INFO> modes(modeCount);
 
-    auto& t = p[0].targetInfo;
+    if (QueryDisplayConfig(
+        QDC_ONLY_ACTIVE_PATHS,
+        &pathCount, paths.data(),
+        &modeCount, modes.data(),
+        nullptr))
+        return 0;
 
-    DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO g{
-        { DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO, sizeof(g), t.adapterId, t.id }
-    };
+    for (auto& path : paths)
+    {
+        auto& t = path.targetInfo;
 
-    if (DisplayConfigGetDeviceInfo(&g.header)) return;
+        DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO g{};
+        g.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO;
+        g.header.size = sizeof(g);
+        g.header.adapterId = t.adapterId;
+        g.header.id = t.id;
 
-    DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{
-        { DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE, sizeof(s), t.adapterId, t.id },
-        !g.advancedColorEnabled
-    };
+        if (DisplayConfigGetDeviceInfo(&g.header))
+            continue;
 
-    if (!DisplayConfigSetDeviceInfo(&s.header))
-        ApplyColorTriggers();
-}
+        if (!g.advancedColorSupported)
+            continue;
 
-LRESULT CALLBACK WndProc(HWND h, UINT m, WPARAM w, LPARAM)
-{
-    if (m == WM_TRAY && w == WM_LBUTTONUP)
-        ToggleHDR();
-    return DefWindowProcW(h, m, w, 0);
-}
+        DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE s{};
+        s.header.type = DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE;
+        s.header.size = sizeof(s);
+        s.header.adapterId = t.adapterId;
+        s.header.id = t.id;
+        s.enableAdvancedColor = !g.advancedColorEnabled;
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int)
-{
-    WNDCLASSW wc{};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInst;
-    wc.lpszClassName = L"HDRTray";
-    RegisterClassW(&wc);
+        if (!DisplayConfigSetDeviceInfo(&s.header))
+            ApplyColorTriggers();
 
-    HWND hwnd = CreateWindowW(wc.lpszClassName, L"", 0,
-        0, 0, 0, 0, HWND_MESSAGE, 0, hInst, 0);
-
-    nid.cbSize = sizeof(nid);
-    nid.hWnd = hwnd;
-    nid.uID = 1;
-    nid.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
-    nid.uCallbackMessage = WM_TRAY;
-    nid.hIcon = LoadIcon(nullptr, IDI_INFORMATION);
-    lstrcpyW(nid.szTip, L"HDR Toggle");
-
-    Shell_NotifyIconW(NIM_ADD, &nid);
-
-    MSG msg;
-    while (GetMessageW(&msg, nullptr, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+        break; // 첫 HDR 지원 디스플레이만
     }
 
-    Shell_NotifyIconW(NIM_DELETE, &nid);
     return 0;
 }
